da1e632d1f02970c242d4f849734077c46dc34fa
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-05-07 21:51:22 +0700

    add migration cats

---
+++ database/migrations/2022_05_07_101843_create_cats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('breed')->nullable();
+            $table->enum('gender',['male','female']);
+            $table->string('color');
+            $table->double('weight',5,2);
+            $table->integer('age');
+            $table->text('story');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+};
