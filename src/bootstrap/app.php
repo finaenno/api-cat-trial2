00726eb04e9e08a76d3e0cd5f30a5f51058de08b
finaenno <finaennorop@student.uns.ac.id>
2022-06-10 01:40:10 +0700

    cancel configuration

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -51,22 +51,5 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-            # [START cloud_sql_mysql_pdo_create_socket]
-            $username = 'catped-user';
-            $password = 'finaenno04';
-            $dbName = 'laravel';
-            $connectionName = getenv("cat-pedigree-trial:asia-southeast2:cat-pedigree-trial");
-            $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
-
-            // Connect using UNIX sockets
-            $dsn = sprintf(
-                'mysql:dbname=%s;unix_socket=%s/%s',
-                $dbName,
-                $socketDir,
-                $connectionName
-            );
-
-            // Connect to the database.
-            $app = new PDO($dsn, $username, $password, $conn_config);
 
 return $app;
